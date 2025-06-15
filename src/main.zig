a475127156e69730b2cf9bd9c963c1bbcdd47e9a
Oly <Oly@yamums.house>
2025-06-15 17:59:29 -0300

    format everything

--- src/main.zig
+++ src/main.zig
@@ -13,7 +13,7 @@ const Pipe = struct {
 };
 
 const maxSpeed = 8;
-const jumpVec = rl.Vector2.init(0, -6);
+const jumpVec = rl.Vector2.init(0, -3);
 const gravity = rl.Vector2.init(0, 0.1);
 
 const Bird = struct {
@@ -21,16 +21,24 @@ const Bird = struct {
     speed: rl.Vector2,
     isDead: bool = false,
 
-    fn getCollisionBox(self: Bird) rl.Rectangle{
-        return rl.Rectangle.init(self.position.x, self.position.y, 80, 80);
+    fn getCollisionBox(self: Bird) rl.Rectangle {
+        return rl.Rectangle.init(self.position.x, self.position.y, 40, 40);
     }
 };
 
+var touchLast = false;
+
 fn birdShouldJump() bool {
-    return rl.isMouseButtonPressed(rl.MouseButton.left)    or
-           rl.isMouseButtonPressed(rl.MouseButton.right)   or
-           rl.isKeyPressed(rl.KeyboardKey.space)           or
-           rl.getTouchPointCount() > 0;
+    const touch = rl.getTouchPointCount() > 0;
+    const touched = touch != touchLast;
+    touchLast = touch;
+
+    std.log.info("toched: {} \nTouchLast: {} \nTouch: {} \n", .{ touched, touchLast, touch });
+
+    return rl.isMouseButtonPressed(rl.MouseButton.left) or
+        rl.isMouseButtonPressed(rl.MouseButton.right) or
+        rl.isKeyPressed(rl.KeyboardKey.space) or
+        touched;
 }
 
 const GameScreen = enum {
@@ -46,21 +54,23 @@ const GameState = struct {
 
     screen: GameScreen,
 
+    screenHeight: u32 = 0,
+    screenWidth: u32 = 0,
+
     distance: u32 = 0,
     speed: f32 = 1,
     score: u32,
     bird: Bird,
 
     pipes: *std.ArrayList(Pipe),
-    currentPipeCollisions: ?* []rl.Rectangle = null,
+    currentPipeCollisions: ?[]rl.Rectangle = null,
     lastPipeDistance: u32 = 0,
 
-    pub fn getArena(self: GameState) error{OutOfMemory}! *const std.mem.Allocator{
-        if (self.arenaAlloc) |_arenaAlloc| return _arenaAlloc
-        else return error.OutOfMemory;
+    pub fn getArena(self: GameState) error{OutOfMemory}!*const std.mem.Allocator {
+        if (self.arenaAlloc) |_arenaAlloc| return _arenaAlloc else return error.OutOfMemory;
     }
 
-    fn init(allocator: std.mem.Allocator, rand: std.Random, pipes: *std.ArrayList(Pipe), bird: *Bird) GameState {
+    fn init(allocator: std.mem.Allocator, rand: std.Random, pipes: *std.ArrayList(Pipe)) GameState {
         return .{
             .allocator = allocator,
             .rand = rand,
@@ -71,13 +81,18 @@ const GameState = struct {
             .distance = 0,
             .pipes = pipes,
 
-            .bird = bird.*
+            .bird = .{ .speed = rl.Vector2{ .x = 0, .y = 0 }, .position = rl.Vector2.init(64, 450 / 2) },
         };
+    }
+
+    fn restart(self: GameState) GameState {
+        self.pipes.clearAndFree();
+        return GameState.init(self.allocator, self.rand, self.pipes);
     }
 };
 
 // okay, so each pipe needs a position and a bounding box,
-// the bird needs a position a bounding box and a texture. 
+// the bird needs a position a bounding box and a texture.
 // i guess i will have a bird and a array of pipes.
 
 pub fn main() !void {
@@ -86,6 +101,7 @@ pub fn main() !void {
     const screenWidth = 800;
     const screenHeight = 450;
 
+    rl.setConfigFlags(.{ .window_resizable = true });
     rl.initWindow(screenWidth, screenHeight, "Crappy Bird");
     defer rl.closeWindow();
 
@@ -99,21 +115,22 @@ pub fn main() !void {
 
     var pipes = std.ArrayList(Pipe).init(allocator);
 
-    var bird = Bird {
-        .speed = rl.Vector2{.x = 0, .y =  0},
-        .position = rl.Vector2.init(64, screenHeight/2)
-    };
-
-    var state = GameState.init(allocator, rand, &pipes, &bird);
+    var state = GameState.init(allocator, rand, &pipes);
 
     rl.setTargetFPS(60);
-    while (!rl.windowShouldClose()) { 
+    while (!rl.windowShouldClose()) {
         var arena = std.heap.ArenaAllocator.init(allocator);
 
+        state.screenHeight = @intCast(rl.getScreenHeight());
+        state.screenWidth = @intCast(rl.getScreenWidth());
         state.arenaAlloc = &arena.allocator();
 
-        try doGameLogic(&state); 
-        try PaintScreen(&state); 
+        try doGameLogic(&state);
+        try PaintScreen(&state);
+
+        if (state.bird.isDead) {
+            state = state.restart();
+        }
 
         arena.deinit();
         state.currentPipeCollisions = null;
@@ -122,51 +139,27 @@ pub fn main() !void {
 
 fn makePipeCollisions(pipes: *[]Pipe, arena: *const std.mem.Allocator) ?[]rl.Rectangle {
     // creates a array containing the collisions for every pipe according to state.
-    // Should be called every frame. The array is detroyed afterwards. 
+    // Should be called every frame. The array is detroyed afterwards.
 
     if (pipes.len == 0) return null;
 
-    var pipeCollisions = 
-        arena.alloc(rl.Rectangle, pipes.len)
-        catch @panic("Couldn't allocate memory.");
+    var pipeCollisions =
+        arena.alloc(rl.Rectangle, pipes.len) catch @panic("Couldn't allocate memory.");
 
     const height: f32 = @floatFromInt(rl.getScreenHeight());
 
     for (pipes.*, 0..) |pipe, idx| {
-        pipeCollisions[idx] = (
-            if (pipe.up) rl.Rectangle.init(
-                pipe.position.x,
-                0,
-                pipeWidth,
-                pipe.position.y
-            )
-
-            else rl.Rectangle.init(
-                pipe.position.x,
-                pipe.position.y,
-                pipeWidth,
-                height - pipe.position.y
-            )
-        );
+        pipeCollisions[idx] = (if (pipe.up) rl.Rectangle.init(pipe.position.x, 0, pipeWidth, pipe.position.y) else rl.Rectangle.init(pipe.position.x, pipe.position.y, pipeWidth, height - pipe.position.y));
     }
 
     return pipeCollisions;
 }
 
 test "makePipeCollisions" {
-    const pipes = [_]Pipe {
-        .{ 
-            .position = rl.Vector2.init(0,15),
-            .up = false
-        },
-        .{ 
-            .position = rl.Vector2.init(1,10),
-            .up = false
-        },
-        .{ 
-            .position = rl.Vector2.init(2,20),
-            .up = false
-        },
+    const pipes = [_]Pipe{
+        .{ .position = rl.Vector2.init(0, 15), .up = false },
+        .{ .position = rl.Vector2.init(1, 10), .up = false },
+        .{ .position = rl.Vector2.init(2, 20), .up = false },
     };
 
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -174,19 +167,17 @@ test "makePipeCollisions" {
 
     const collisions = makePipeCollisions(&pipes, &arena.allocator());
     _ = collisions;
-    
+
     const expect = std.testing.expect;
 
-    try expect(
-       false
-    );
+    try expect(false);
 }
 
 fn attemptMakeNewPipe(state: *GameState) void {
-    const width: f32 =  @floatFromInt(rl.getScreenWidth());
+    const width: f32 = @floatFromInt(rl.getScreenWidth());
     const height: f32 = @floatFromInt(rl.getScreenHeight());
 
-    const amountBeforePipe = pipeWidth + 20;
+    const amountBeforePipe = 300;
 
     if ((state.distance - state.lastPipeDistance) >= amountBeforePipe) {
         state.lastPipeDistance = state.distance;
@@ -194,34 +185,24 @@ fn attemptMakeNewPipe(state: *GameState) void {
         const openingLength = @max((height * 0.75) * state.rand.float(f32), height * 0.25);
         const openingDistanceGround = (height - openingLength) * state.rand.float(f32);
 
-        state.pipes.append(
-            Pipe {
-                .position = rl.Vector2.init(
-                    @floor(width),
-                    @floor(height - openingDistanceGround),
-                ),
-                .up = false
-            }
-        ) catch @panic("cannot allocate memory");
+        state.pipes.append(Pipe{ .position = rl.Vector2.init(
+            @floor(width),
+            @floor(height - openingDistanceGround),
+        ), .up = false }) catch @panic("cannot allocate memory");
 
-        state.pipes.append(
-            Pipe {
-                .position = rl.Vector2.init(
-                    @floor(width),
-                    @floor(height - (openingLength + openingDistanceGround)),
-                ),
-                .up = true
-            }
-        ) catch @panic("cannot allocate memory");
+        state.pipes.append(Pipe{ .position = rl.Vector2.init(
+            @floor(width),
+            @floor(height - (openingLength + openingDistanceGround)),
+        ), .up = true }) catch @panic("cannot allocate memory");
     }
 }
 
 fn doGameLogic(state: *GameState) !void {
     var bird = &state.bird;
 
-    state.speed = @max(
-        @as(f32, @floatFromInt(state.distance)) / 1000,
-        state.speed
+    state.speed = @min(
+        @max(@as(f32, @floatFromInt(state.distance)) / 2000, state.speed),
+        10,
     );
 
     state.distance = state.distance + @as(u32, @intFromFloat(state.speed));
@@ -232,7 +213,9 @@ fn doGameLogic(state: *GameState) !void {
 
     const res = rl.Vector2.add(bird.position, bird.speed);
 
-    if (res.y < -15) { bird.speed = bird.speed.subtract(jumpVec); }
+    if (res.y < -15) {
+        bird.speed = bird.speed.subtract(jumpVec);
+    }
     bird.position = res;
 
     attemptMakeNewPipe(state);
@@ -245,15 +228,17 @@ fn doGameLogic(state: *GameState) !void {
     const arena = state.getArena() catch @panic("Failed to get Allocator");
 
     var pipes = makePipeCollisions(&state.pipes.items, arena);
-    if (pipes) |*p| {
-        state.currentPipeCollisions = p;
+    state.currentPipeCollisions = pipes;
+
+    if (state.bird.position.y > @as(f32, @floatFromInt(state.screenHeight))) {
+        bird.isDead = true;
+    } else if (pipes) |*p| {
         const birdCollision = bird.getCollisionBox();
-        
-        for (p.*) | pipe | {
+
+        for (p.*) |pipe| {
             bird.isDead = rl.checkCollisionRecs(pipe, birdCollision) or bird.isDead;
         }
     }
-
 }
 
 fn PaintScreen(state: *GameState) !void {
@@ -262,44 +247,38 @@ fn PaintScreen(state: *GameState) !void {
     rl.beginDrawing();
     defer rl.endDrawing();
 
-    rl.clearBackground(rl.Color.red);
+    rl.clearBackground(rl.Color.white);
 
     const birdCollision = state.bird.getCollisionBox();
 
     if (state.pipes.items.len > 0) {
-        const pipeCollisions = 
-            if (state.currentPipeCollisions) |col| col.*
-            else @panic("Pipe collisions have not been made.");
-    
-        for (state.pipes.items, 0..) |pipe, idx| {
+        const pipeCollisions =
+            if (state.currentPipeCollisions) |col| col else @panic("Pipe collisions have not been made.");
+
+        for (state.pipes.items, 0..) |_, idx| {
             rl.drawRectangleRec(
                 pipeCollisions[idx],
-                if (pipe.up) rl.Color.purple else rl.Color.green
+                rl.Color.red,
             );
         }
     }
 
-    rl.drawRectangleRec(birdCollision, if (state.bird.isDead) rl.Color.orange else rl.Color.gold);
+    rl.drawRectangleRec(birdCollision, if (state.bird.isDead) rl.Color.orange else rl.Color.black);
 
-    const str: [:0]u8 = @ptrCast(
-        try std.fmt.allocPrint(
-            arenaAlloc,
-            "Distance: {d:.3} \nSpeed: {d:.3} \nScore: {}",
-            .{state.distance, state.speed, state.score}
-    ));
+    const str: [:0]u8 = @ptrCast(try std.fmt.allocPrint(arenaAlloc, "Distance: {d:.3} \nSpeed: {d:.3} \nScore: {}", .{ state.distance, state.speed, state.score }));
 
     rl.drawText(str, 16, 16, 16, .black);
 }
 
 /// custom panic handler for Android
-pub const panic = 
-    if (builtin.abi.isAndroid()) android.panic
-    else std.debug.FullPanic(std.debug.defaultPanic);
+pub const panic =
+    if (builtin.abi.isAndroid()) android.panic else std.debug.FullPanic(std.debug.defaultPanic);
 
 /// custom standard options for Android
-pub const std_options: std.Options = 
-    if (builtin.abi.isAndroid()) .{ .logFn = android.logFn, }
-    else .{};
+pub const std_options: std.Options =
+    if (builtin.abi.isAndroid()) .{
+        .logFn = android.logFn,
+    } else .{};
 
 comptime {
     if (builtin.abi.isAndroid()) {
@@ -309,4 +288,6 @@ comptime {
     }
 }
 
-fn androidMain() callconv(.c) c_int { return std.start.callMain(); }
+fn androidMain() callconv(.c) c_int {
+    return std.start.callMain();
+}
